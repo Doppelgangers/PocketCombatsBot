5fa688e096d95f2562925706007b83f50d65b97e
Doppelgangers <6666genocide666@mail.ru>
2022-10-21 21:52:52 +0300

    Добавил возможность боту нападать на врагов

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ import mss
 from ahk import AHK
 import requests
 
-import mouse
 
 class TelegramBot:
     api_token = ""
@@ -58,45 +57,51 @@ class Window_manager:
 
 class Template:
 
+    def __init__(self, path_image_template):
+        self.img: numpy.ndarray = cv2.imread(path_image_template, cv2.IMREAD_GRAYSCALE)
+
+
+class Template_Enemy(Template):
+
     def __init__(self, name, path_image_template):
-        name: str = ""
-        img: numpy.ndarray = cv2.imread(path_image_template, cv2.IMREAD_GRAYSCALE)
+        super().__init__(path_image_template)
+        self.name: str = name
 
 
-class Templates_Enemies(Template):
+class Template_Skill(Template):
 
-    fire_wood = cv2.imread('assets/Templates/Enemies/fire_wood.png', cv2.IMREAD_GRAYSCALE)
-    fire_spider = cv2.imread('assets/Templates/Enemies/fire_spider.png', cv2.IMREAD_GRAYSCALE)
-    sceleton = cv2.imread('assets/Templates/Enemies/sceleton.png', cv2.IMREAD_GRAYSCALE)
-    mini_snake = cv2.imread('assets/Templates/Enemies/mini_snake.png', cv2.IMREAD_GRAYSCALE)
-    hydra = cv2.imread('assets/Templates/Enemies/hydra.png', cv2.IMREAD_GRAYSCALE)
-    witch = cv2.imread('assets/Templates/Enemies/witch.png', cv2.IMREAD_GRAYSCALE)
-    mumu = cv2.imread('assets/Templates/Enemies/mumu.png', cv2.IMREAD_GRAYSCALE)
-    sand_man = cv2.imread('assets/Templates/Enemies/sand_man.png', cv2.IMREAD_GRAYSCALE)
-    skorpion = cv2.imread('assets/Templates/Enemies/skorpion.png', cv2.IMREAD_GRAYSCALE)
-    noob = cv2.imread("assets/Templates/Enemies/noob.png", cv2.IMREAD_GRAYSCALE)
-    evil_wood = cv2.imread("assets/Templates/Enemies/evil_wood.png", cv2.IMREAD_GRAYSCALE)
-    forest_wolf = cv2.imread("assets/Templates/Enemies/forest_wolf.png", cv2.IMREAD_GRAYSCALE)
-
-class Templates_Skills:
-    pass
+    def __init__(self, path_image_template):
+        super().__init__(path_image_template)
 
 
-class Templates_UI:
-    attack_the_enemy = cv2.imread("assets/Templates/UI/attack_the_enemy.png", cv2.IMREAD_GRAYSCALE)
+class Template_UI(Template):
+
+    def __init__(self, path_image_template):
+        super().__init__(path_image_template)
+
+
+class UI:
+    attack_the_enemy = Template_UI("assets/Templates/UI/attack_the_enemy.png")
+
+
+class Enemies:
+    noob = Template_Enemy("Попрашайка", "assets/Templates/Enemies/noob.png")
+    evil_wood = Template_Enemy("Злобный древень", "assets/Templates/Enemies/evil_wood.png")
+    forest_wolf = Template_Enemy("Молодой волк", "assets/Templates/Enemies/forest_wolf.png")
 
 
 class Finder:
 
     @staticmethod
-    def find_object(template, img_gray, precision: float = 0.6, method=cv2.TM_CCOEFF_NORMED, draw_rect_in_gray_img: bool = False):
+    def find_object(template, img_gray, precision: float = 0.6, method=cv2.TM_CCOEFF_NORMED,
+                    draw_rect_in_gray_img: bool = False):
 
         position_find_object = {}
         width_template, height_template = template.shape[::-1]
-        
+
         result = cv2.matchTemplate(img_gray, template, method)
         location_of_matches = np.where(result >= precision)
-        
+
         for pt in zip(*location_of_matches[::-1]):
 
             if draw_rect_in_gray_img:
@@ -104,35 +109,52 @@ class Finder:
 
             # Координаты найденного обекта, относительно обасти видимости opencv (monitor)
             position_find_object = {
-                "lt_X": pt[0],                      # Лево вверх, координаты X
-                "lt_Y": pt[1],                      # Лево вверх, координаты Y
-                "rb_X": pt[0] + width_template,     # Право низ, координаты X
-                "rb_Y": pt[1] + height_template     # Право низ, координаты Y
+                "lt_X": pt[0],  # Лево вверх, координаты X
+                "lt_Y": pt[1],  # Лево вверх, координаты Y
+                "rb_X": pt[0] + width_template,  # Право низ, координаты X
+                "rb_Y": pt[1] + height_template  # Право низ, координаты Y
             }
             break
 
         return position_find_object
 
+    @staticmethod
+    def find_in_object(template, img_gray, y1, y2, precision: float = 0.6, draw_rect_in_gray_img: bool = False):
+        cut_img_gray = img_gray[y1:y2]
+
+        local_position = Finder.find_object(template=template, img_gray=cut_img_gray, precision=precision, draw_rect_in_gray_img=draw_rect_in_gray_img)
+        if local_position:
+            local_position["lt_Y"] += y1
+            local_position["rb_Y"] += y1
+            return local_position
+
 
 class Actions_Character:
 
-    @staticmethod
-    def click_to(position):
-        if position:
-            pag.moveTo("""""")
-            pag.mouseDown()
-            time.sleep( random.randrange(31, 109) / 1000 )
-            pag.mouseUp()
-            return True
+    def __init__(self, monitor_manager):
+        self.monitor_manager = monitor_manager
 
     @staticmethod
-    def find_fight(img_gray, enemy):
-        enemy_position = Finder.find_object(template=enemy, img_gray=img_gray, precision=0.8, draw_rect_in_gray_img=True)
+    def click_to(x, y):
+        pag.moveTo(x, y, 0.05)
+        pag.mouseDown()
+        time.sleep(random.randrange(31, 109) / 1000)
+        pag.mouseUp()
+
+    def find_fight(self, img_gray, enemy):
+        enemy_position = Finder.find_object(template=enemy.img, img_gray=img_gray, precision=0.8,
+                                            draw_rect_in_gray_img=True)
         if enemy_position:
-            enemy_area = img_gray[enemy_position["lt_Y"]:enemy_position["rb_Y"]]
-            attack_position = Finder.find_object(template=Templates_UI.attack_the_enemy, img_gray=enemy_area, precision=0.7, draw_rect_in_gray_img=True)
-            print("Напал", attack_position)
-            return attack_position
+            attack_position = Finder.find_in_object(UI.attack_the_enemy.img, img_gray, enemy_position["lt_Y"], enemy_position["rb_Y"])
+            if attack_position:
+                self.start_a_fight(attack_position)
+        return True
+
+
+    def start_a_fight(self, attack_position: dict):
+        x = random.randint(attack_position.get("lt_X"), attack_position.get("rb_X"))
+        y = random.randint(attack_position.get("lt_Y"), attack_position.get("rb_Y"))
+        Actions_Character.click_to(self.monitor_manager.monitor.get("left") + x, self.monitor_manager.monitor.get("top") + y)
 
 
 def main_loop():
@@ -143,11 +165,10 @@ def main_loop():
             img = np.asarray(screenshot.grab(monitor_manager.monitor))
             img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-            first = Actions_Character.find_fight(enemy=Templates_Enemies.evil_wood, img_gray=img_gray)
-            second = Actions_Character.find_fight(enemy=Templates_Enemies.forest_wolf, img_gray=img_gray)
+            actions = Actions_Character(monitor_manager)
 
-
-
+            first = actions.find_fight(enemy=Enemies.forest_wolf, img_gray=img_gray)
+            second = actions.find_fight(enemy=Enemies.evil_wood, img_gray=img_gray)
 
             cv2.imshow("BOT", img_gray)
 
